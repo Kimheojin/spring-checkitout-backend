a4cc0f2b10f8774cd01fe2b9aed896c816dcdd59
jin <hurjin1109@naver.com>
2024-08-05 23:17:45 +0900

    나머지 로직

---
+++ src/main/java/hongik/demo_book/apicontroller/BookController.java
@@ -0,0 +1,7 @@
+package hongik.demo_book.apicontroller;
+
+
+//아직 구현 X
+//로그인 비밀번호 변경 + 주소까지 반환 만들고 그거하기
+public class BookController {
+}
--- src/main/java/hongik/demo_book/apicontroller/MemberController.java
+++ src/main/java/hongik/demo_book/apicontroller/MemberController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api")
 public class MemberController {
+
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
