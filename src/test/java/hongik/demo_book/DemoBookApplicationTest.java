2aaf15e54852b2c4ee942d58bb4794a9a4081886
jin <hurjin1109@naver.com>
2025-04-07 22:48:15 +0900

    TEST : test 코드 추가

--- src/test/java/hongik/demo_book/DemoBookApplicationTest.java
+++ src/test/java/hongik/demo_book/DemoBookApplicationTest.java
@@ -1,15 +1,14 @@
-package Hongik.demo_book;
+package hongik.demo_book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
-
-@SpringBootTest
-@Profile("dev")
-class DemoBookApplicationTest {
+import static org.junit.jupiter.api.Assertions.*;
 
 
+@SpringBootTest(classes = DemoBookApplication.class)
+public class DemoBookApplicationTest {
     @Test
     void contextLoads() {
     }
