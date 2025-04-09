3a52ed0f1454b4ba38430d08dea215db177331f1
jin <hurjin1109@naver.com>
2025-04-09 15:17:41 +0900

    FIX : MemberAuthorities 저장 문제 수정
    - Transactional 걸려있지 않아 저장이 기존 로직과 상이하게 적용되던 문제 수정

--- src/test/java/hongik/demo_book/config/DemoMockSecurityContext.java
+++ src/test/java/hongik/demo_book/config/DemoMockSecurityContext.java
@@ -6,25 +6,22 @@ import hongik.demo_book.Repository.MemberRepository;
 import hongik.demo_book.domain.Authority;
 import hongik.demo_book.domain.Member;
 import hongik.demo_book.domain.MemberAuthority;
+import hongik.demo_book.exception.NoRoleUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-public class DemoMockSecurityContext implements WithSecurityContextFactory<WithMockUser> {
+public class DemoMockSecurityContext implements WithSecurityContextFactory<WithMockBookUser> {
 
     private final MemberRepository memberRepository;
     private final AuthorityRepository authorityRepository;
@@ -32,10 +29,10 @@ public class DemoMockSecurityContext implements WithSecurityContextFactory<WithM
 
 
     @Override
-    public SecurityContext createSecurityContext(WithMockUser annotation) {
-        String email = annotation.value(); // annotation 한번 가공 되어서 value로
+    public SecurityContext createSecurityContext(WithMockBookUser annotation) {
+        String email = annotation.email(); // annotation 한번 가공 되어서 value로
         String password = annotation.password();
-        String membername = annotation.username(); // 마찬가지 membername
+        String membername = annotation.membername(); // 마찬가지 membername
         String[] roles = annotation.roles();
 
 
@@ -50,8 +47,6 @@ public class DemoMockSecurityContext implements WithSecurityContextFactory<WithM
                 .collect(Collectors.toList());
 
 
-        User principal = new User(testMember.getEmail(), testMember.getPassword(), authorities);
-
 
         Authentication auth = new UsernamePasswordAuthenticationToken(testMember.getEmail(), null, authorities);
         context.setAuthentication(auth);
@@ -59,21 +54,13 @@ public class DemoMockSecurityContext implements WithSecurityContextFactory<WithM
         return context;
     }
 
-    private Member createMember(String email, String password, String membername, String[] roles) {
 
 
-        Set<MemberAuthority> memberAuthorities = new HashSet<>();
+    private Member createMember(String email, String password, String membername, String[] role) {
 
-        for (String role : roles) {
-            Authority authority = authorityRepository.findByAuthorityName(role)
-                    .orElseGet(() -> authorityRepository.save(Authority.builder().authorityName(role).build()));
 
-            MemberAuthority memberAuthority = MemberAuthority.builder()
-                    .authority(authority)
-                    .build();
-
-            memberAuthorities.add(memberAuthority);
-        }
+        Authority authority = authorityRepository.findByAuthorityName(role[0]).orElseThrow(
+                () -> new NoRoleUser());
 
 
         Member member = Member.builder()
@@ -81,17 +68,16 @@ public class DemoMockSecurityContext implements WithSecurityContextFactory<WithM
                 .password(passwordEncoder.encode(password))
                 .membername(membername)
                 .activated(true)
-                .memberAuthorities(memberAuthorities)
                 .build();
 
-        for (MemberAuthority memberAuthority : memberAuthorities) {
-            memberAuthority = MemberAuthority.builder()
-                    .member(member)
-                    .authority(memberAuthority.getAuthority())
-                    .build();
-        }
 
+        MemberAuthority memberAuthority = MemberAuthority.builder()
+                .authority(authority)
+                .member(member)  // Member 설정
+                .build();
+        member.getMemberAuthorities().add(memberAuthority);
+        member = memberRepository.save(member);
         // 저장 후 반환
-        return memberRepository.save(member);
+        return member;
     }
 }
