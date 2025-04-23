2950e3c5f21729f298d3f11f08a5793f672f19e9
jin <hurjin1109@naver.com>
2025-04-23 17:21:39 +0900

    FIX : 도서 저장 로직 오류 변경

--- src/main/java/hongik/demo_book/service/BookService.java
+++ src/main/java/hongik/demo_book/service/BookService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -98,25 +99,25 @@ public class BookService {
 
         Member member = customUserService.GetCurrentMember();
 
-        List<Category> categories = categoryRepository.findCategoriesWithMember(member);
 
-        Category categoryToUpdate = categories.stream()
-                .filter(category -> category.getCategoryName().equals(bookdto.getCategoryName()))
-                .findFirst()
-                .orElseThrow(NotFoundCategory::new);
 
-        if (categoryToUpdate == null) {
-            categoryToUpdate= Category.builder()
-                    .categoryName(bookdto.getCategoryName())
-                    .member(member)
-                    .build();
 
-            categoryRepository.save(categoryToUpdate);
-        }
+        // 이미 존재하는 겨우
+        Category category = categoryRepository.findCategoriesWithMember(member).stream()
+                .filter(c -> c.getCategoryName().equals(bookdto.getCategoryName()))
+                .findFirst().orElseGet(
+                        () -> {
+                            Category newCategory = Category.builder()
+                                    .categoryName(bookdto.getCategoryName())
+                                    .member(member)
+                                    .build();
+                            return categoryRepository.save(newCategory);
+                        });
+
 
         Book book = Book.builder()
                 .isbn13(bookdto.getIsbn13())
-                .category(categoryToUpdate)
+                .category(category)
                 .build();
 
         bookRepository.save(book);
